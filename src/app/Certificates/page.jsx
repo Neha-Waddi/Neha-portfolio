4ae94075417a35c122dceac005e572d9d2cbb4ea
Neha-Waddi <waddineha.22.csm@anits.edu.in>
2024-12-15 20:52:34 +0530

    ..

--- src/app/Certificates/page.jsx
+++ src/app/Certificates/page.jsx
@@ -8,6 +8,28 @@ const page = () => {
         <Navbar/>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:ml-20 sm:ml-20'>
         <Image 
+          src="/images/int_cert.jpg"
+          width={300}
+          height={300}
+          className='m-20  shadow-md hover:shadow-lg transition duration-300 ease-in-out  hover:scale-110  '
+          style={{ 
+          
+            border: '1px solid #000000', 
+          }}
+          
+        />
+        <Image 
+          src="/images/aws_cert.jpg"
+          width={300}
+          height={300}
+          className='m-20  shadow-md hover:shadow-lg transition duration-300 ease-in-out  hover:scale-110  '
+          style={{ 
+          
+            border: '1px solid #000000', 
+          }}
+          
+        />
+        <Image 
           src="/images/Screenshot 2024-05-19 173306.png"
           width={300}
           height={300}
--- src/app/Projects/page.jsx
+++ src/app/Projects/page.jsx
@@ -9,21 +9,23 @@ const Projects = () => {
       <section className="projects p-16">
         <h2 className="font-bold text-2xl mb-7 text-center">My Projects</h2>
         <div className="project-container grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div className="project-card bg-white shadow rounded-lg p-4 hover:shadow-lg transition duration-300 ease-in-out hover:scale-105 hover:bg-slate-300">
+        <div className="project-card bg-white shadow rounded-lg p-4 hover:shadow-lg transition duration-300 ease-in-out hover:scale-105 hover:bg-slate-300">
             <Image
-              src="/images/Screenshot 2024-04-06 184451.png"
+              src="/images/domain_proj_1pic.jpg"
               alt="Know the Unknown Project Screenshot"
               height={270}
               width={270}
               className="rounded-lg object-cover ml-44 border-2 border-black" 
             />
-            <h3 className="text-black font-semibold text-center mt-4">Know the Unknown</h3>
+            <h3 className="text-black font-semibold text-center mt-4">Domain Mapping</h3>
             <p className="text-black">
-              A website dedicated to sharing scientific updates. Built with HTML, CSS, and JavaScript to create an interactive and visually appealing platform. Users can explore and learn about various scientific topics and discoveries.
+              A platform for Domain & URL mapping with signUP,authentication and editing functionalities.
+              <br></br>
+              <p><strong>Technologies used :</strong>Nextjs , TailwindCSS , MongoDB</p>
             </p>
             <a
               className="hover:underline text-blue-500"
-              href="https://neha-waddi.github.io/knowtheunknown.github.io/"
+              href="https://github.com/Neha-Waddi/domain_proj_nextjs"
               target="_blank"
               rel="noopener noreferrer" 
             >
@@ -41,13 +43,39 @@ const Projects = () => {
             />
             <h3 className="text-black font-semibold text-center mt-4">My Personal Portfolio</h3>
             <p className="text-black">
-              A website that showcases my technical skills and experiences. Built with Next.js and Tailwind CSS to create an interactive and visually appealing platform.
+              A website that showcases my technical skills and experiences.
             </p>
+            <br></br>
+            <p className='text-black'><strong>Technologies used :</strong>Nextjs , TailwindCSS</p>
             <a
               className="hover:underline text-blue-500"
               href="https://neha-waddi.github.io/knowtheunknown.github.io/" 
               target="_blank"
               rel="noopener noreferrer"
+            >
+              Link to project
+            </a>
+          </div>
+          
+          <div className="project-card bg-white shadow rounded-lg p-4 hover:shadow-lg transition duration-300 ease-in-out hover:scale-105 hover:bg-slate-300">
+            <Image
+              src="/images/Screenshot 2024-04-06 184451.png"
+              alt="Know the Unknown Project Screenshot"
+              height={270}
+              width={270}
+              className="rounded-lg object-cover ml-44 border-2 border-black" 
+            />
+            <h3 className="text-black font-semibold text-center mt-4">Know the Unknown</h3>
+            <p className="text-black">
+              A website dedicated to sharing scientific updates. Built with HTML, CSS, and JavaScript to create an interactive and visually appealing platform. Users can explore and learn about various scientific topics and discoveries.
+            </p>
+            <br></br>
+            <p className='text-black'><strong>Technologies used :</strong>HTML , CSS , JavaScript</p>
+            <a
+              className="hover:underline text-blue-500"
+              href="https://neha-waddi.github.io/knowtheunknown.github.io/"
+              target="_blank"
+              rel="noopener noreferrer" 
             >
               Link to project
             </a>
