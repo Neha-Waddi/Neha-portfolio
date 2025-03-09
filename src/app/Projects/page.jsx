26fdde7c8f754781bfdd552ef022e4c2e4dc75a2
Neha-Waddi <waddineha.22.csm@anits.edu.in>
2025-03-09 11:18:06 +0530

    added a project

--- src/app/Projects/page.jsx
+++ src/app/Projects/page.jsx
@@ -6,13 +6,13 @@ const Projects = () => {
   return (
     <>
       <Navbar />
-      <section className="projects p-16">
-        <h2 className="font-bold text-2xl mb-7 text-center">My Projects</h2>
+      <section className="projects p-16"  style={{ backgroundImage: "url('/images/proj_bg.png')" }}>
+        <h2 className="font-bold text-2xl mb-7 text-center text-white">My Projects</h2>
         <div className="project-container grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="project-card bg-white shadow rounded-lg p-4 hover:shadow-lg transition duration-300 ease-in-out hover:scale-105 hover:bg-slate-300">
             <Image
               src="/images/domain_proj_1pic.jpg"
-              alt="Know the Unknown Project Screenshot"
+              alt="domain Project Screenshot"
               height={270}
               width={270}
               className="rounded-lg object-cover ml-44 border-2 border-black" 
@@ -21,7 +21,7 @@ const Projects = () => {
             <p className="text-black">
               A platform for Domain & URL mapping with signUP,authentication and editing functionalities.
               <br></br>
-              <p><strong>Technologies used :</strong>Nextjs , TailwindCSS , MongoDB</p>
+              <span><strong>Technologies used :</strong>Nextjs , TailwindCSS , MongoDB</span>
             </p>
             <a
               className="hover:underline text-blue-500"
@@ -32,10 +32,34 @@ const Projects = () => {
               Link to project
             </a>
           </div>
+          <div className="project-card bg-white shadow rounded-lg p-4 hover:shadow-lg transition duration-300 ease-in-out hover:scale-105 hover:bg-slate-300">
+            <Image
+              src="/images/frndfd_ss.png"
+              alt="friendfedd proj Screenshot"
+              height={270}
+              width={270}
+              className="rounded-lg object-cover ml-44 border-2 border-black" 
+            />
+            <h3 className="text-black font-semibold text-center mt-4">My Personal Portfolio</h3>
+            <p className="text-black">
+            FriendFeed is a Social Networking Site built using PHP
+            </p>
+            <br></br>
+            <p className='text-black'><strong>Technologies used :</strong>Php , MySQL , HTML , CSS</p>
+            <a
+              className="hover:underline text-blue-500"
+              href="https://github.com/Neha-Waddi/FriendFeed" 
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Link to project
+            </a>
+          </div>
+          
 
           <div className="project-card bg-white shadow rounded-lg p-4 hover:shadow-lg transition duration-300 ease-in-out hover:scale-105 hover:bg-slate-300">
             <Image
-              src="/images/Screenshot 2024-06-05 185740.png"
+              src="/images/prtflio.png"
               alt="Personal Portfolio Project Screenshot"
               height={270}
               width={270}
@@ -49,7 +73,7 @@ const Projects = () => {
             <p className='text-black'><strong>Technologies used :</strong>Nextjs , TailwindCSS</p>
             <a
               className="hover:underline text-blue-500"
-              href="https://neha-waddi.github.io/knowtheunknown.github.io/" 
+              href="https://github.com/Neha-Waddi/Neha-portfolio" 
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -59,7 +83,7 @@ const Projects = () => {
           
           <div className="project-card bg-white shadow rounded-lg p-4 hover:shadow-lg transition duration-300 ease-in-out hover:scale-105 hover:bg-slate-300">
             <Image
-              src="/images/Screenshot 2024-04-06 184451.png"
+              src="/images/know.png"
               alt="Know the Unknown Project Screenshot"
               height={270}
               width={270}
