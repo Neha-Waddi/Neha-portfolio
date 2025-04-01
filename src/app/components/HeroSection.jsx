8ebfc3b3ee2357f9dd3ccf194b92d02a7c4cff0d
Neha-Waddi <waddineha.22.csm@anits.edu.in>
2025-04-01 23:37:46 +0530

    added alert

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,7 +7,6 @@ import { FaGithub, FaLinkedin, FaHackerrank } from "react-icons/fa";
 const HeroSection = () => {
   return (
     <section className="relative flex flex-col items-center justify-center py-16 lg:py-4 text-white overflow-hidden">
-      {/* Video Background */}
       <div className="fixed top-0 left-0 w-full h-full z-0">
         <video
           autoPlay
@@ -19,7 +18,6 @@ const HeroSection = () => {
         </video>
       </div>
 
-      {/* Content */}
       <h1 className="text-4xl lg:text-6xl font-bold mb-4 z-10">
         <TypeAnimation
           className="inline-block bg-clip-text text-transparent bg-cyan-400" // Changed gradient for better visibility
@@ -57,7 +55,6 @@ const HeroSection = () => {
         />
       </div>
 
-      {/* Social Media Links */}
       <div className="flex justify-center mt-8 lg:mt-16 z-10">
       <a href="https://github.com/Neha-Waddi" target="_blank" rel="noopener noreferrer">
   <FaGithub className="text-4xl hover:scale-110 transition duration-300 ease-in-out mx-4 h-12 w-12 text-black bg-white rounded-full p-2 shadow-md" />
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,26 @@
-import Image from "next/image";
-import HeroSection  from "./components/HeroSection";
+"use client";
+import { useEffect, useState } from "react";
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
-
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+    if (window.innerWidth < 768) {
+      alert("Use a laptop for a better experience!");
+    }
+  }, []);
+
+  if (!isClient) return null; 
+
   return (
     <main className="flex min-h-screen flex-col bg-[#ffffff]"> 
-    <Navbar />
-    <div className="container  mx-auto px-12 py-6">
-      <HeroSection />
-      
-    </div>
+      <Navbar />
+      <div className="container mx-auto px-12 py-6">
+        <HeroSection />
+      </div>
     </main>
   );
 }
