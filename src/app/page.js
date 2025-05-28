660d766bb3ee114ff38f93d155d39a09648fd5ee
Neha-Waddi <waddineha.22.csm@anits.edu.in>
2025-05-28 22:20:26 +0530

    logo and preloader

--- src/app/page.js
+++ src/app/page.js
@@ -6,15 +6,28 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
     const [isClient, setIsClient] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         setIsClient(true);
         if (window.innerWidth < 768) {
             alert("Use a laptop for a better experience!");
         }
+
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 5000);
+
+        return () => clearTimeout(timer);
     }, []);
 
-    if (!isClient) return null;
+    if (!isClient || isLoading) {
+        return (
+            <div className="fixed inset-0 bg-white flex items-center justify-center z-50">
+                <img src="/loader.gif" alt="Loading..." className="w-40 h-40" />
+            </div>
+        );
+    }
 
     return (
         <main className="flex min-h-screen bg-[#ffffff]">
